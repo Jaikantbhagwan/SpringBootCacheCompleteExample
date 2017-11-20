9145a3e5dce2edfce664ddb7a04cc389f262f825
Jaikant S <jaikant_singh@apple.com>
2017-11-20 19:06:09 +0530

    First Commit

---
+++ src/main/java/com/jaikant/sampleapp/cache/SpringBootCacheCompleteExampleApplication.java
@@ -0,0 +1,12 @@
+package com.jaikant.sampleapp.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCacheCompleteExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCacheCompleteExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/org/springframework/Spring_Boot_Cache_Complete_Example/SpringBootCacheCompleteExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package org.springframework.Spring_Boot_Cache_Complete_Example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootCacheCompleteExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
